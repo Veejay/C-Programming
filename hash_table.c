1ad6c398a308ccdd9ba6413000bb5d798b087c3c
Bertrand Chardon <bertrand.chardon@gmail.com>
2012-04-27 16:28:15 -0700

    Added linked list functions
    As a result the functions on hashtables are basically a call to the
    adequate linked list function on the proper bucket, as determined by the
    hash function.
    
    TODO:
    ----
    
    - Constants have not been created yet, code won't compile as is
    - All linked list-related code should be put in a separate file with the
      proper includes.
    - malloc code should be error-checked

--- hash_table.c
+++ hash_table.c
@@ -30,13 +30,6 @@ int hash(char *key, int size){
   return hash % size;
 }
 
-/*
- *
- * ACCESS OPERATIONS
- *
- *
- */
-
 // Store an element in the hashtable.
 // Should return a status code to give information about the operation I guess...
 // If the key already exists in the hash table, updates the record 
@@ -44,35 +37,63 @@ int hash(char *key, int size){
 int put(hashtable *ht, key_value* kv)
 {
   // First we need to determinate the proper hash for that key/value pair
-  /* char *key = kv->key; */ 
-  /* int hashed_value = hash(key, ht->size); */
-
+  char *key = kv->key; 
+  int hashed_value = hash(key, ht->size);
+  
   // Now we can access the proper bucket and store the data
+  // FIXME: This is extra ugly (ideally the inser function 
+  // returns a status code but I couldn't make that work)
+  ht->table[hashed_value] = linked_list_insert(ht->table[hashed_value], *kv);
   return 0; 
 }
 
 // Retrieves the key_value stored in the hash table based on the key hash
-key_value* get(hashtable *ht, char *key)
+key_value get(hashtable *ht, char *key)
 {
-  return NULL; 
+  int hashed_value = hash(key, ht->size); 
+  return linked_list_get(ht->table[hashed_value], key);
 } 
 
 // Deletes the record associated to the key in the hash table
 int delete(hashtable *ht, char *key)
 {
-  return 0; 
-
+  int hashed_value = hash(key, ht->size); 
+  return linked_list_delete(ht->table[hashed_value], key); 
 }
 
 /************************* HELPERS ************************************/
 
-int linked_list_insert(linked_list *list, key_value kv){
-  linked_list *head = malloc(sizeof(linked_list));
-  head->kv = kv;
-  head->next = list;
-  list = head;
-  free(head);
-  return 0;
+int linked_list_insert(linked_list **list, key_value kv){
+  linked_list *tmp = *list; 
+  *list = malloc(sizeof(linked_list));
+  (*list)->kv = kv;
+  (*list)->next = tmp;
+  return 0; 
+}
+
+key_value linked_list_get(linked_list *l, char *key)
+{
+  while(strcmp(l->kv.key, key) != 0){
+    l = l->next;
+  }
+  return l->kv;
+}
+
+int linked_list_delete(linked_list *l, char *key)
+{
+  while(l->next != NULL && strcmp(l->kv.key, key) != 0){
+    l = l->next;
+  }
+  if(l->next == NULL && (strcmp(l->kv.key, key) != 0)){
+    return LL_KEY_NOT_FOUND;
+  }
+  else{
+    linked_list *tmp = l->next;
+    l->kv = tmp->kv;
+    l->next = tmp->next;
+    free(tmp);
+    return LL_DELETE_OK;
+  }
 }
 
 // Mainly used for testing of insert function
@@ -80,7 +101,7 @@ void print_linked_list(linked_list *list)
 {
   int i = 0;
   while((list)){
-    printf("Element #%d: %s\n", i, list->kv.value);
+    printf("Element #%d: {key:\"%s\", value: \"%s\"}\n", i, list->kv.key, list->kv.value);
     list = list->next;
     i++;
   }
@@ -90,14 +111,11 @@ void print_linked_list(linked_list *list)
 
 int main()
 {
-  key_value kv1 = {.key="foo", .value="Bertrand Chardon"};
-  key_value kv2 = {.key="goo", .value="Bertrand Chrdon"};
-  key_value kv3 = {.key="hoo", .value="Bertrand hardon"};
-
+  key_value kv1 = {.key="Bertrand", .value="Chardon"};
+  key_value kv2 = {.key="Guillaume", .value="Leconte"};
   linked_list *l = NULL;
-
-  linked_list_insert(l, kv1);
-  print_linked_list(l); 
-
+  linked_list_insert(&l, kv1);
+  linked_list_insert(&l, kv2);
+  print_linked_list(l);
   return 0;
 }
